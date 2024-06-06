37b86df232053f7a09da109b6782057d5deff1ea
Elvis Cops <elvis.cops93@gmail.com>
2024-06-06 21:07:09 +0200

    Exercise 5.7

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,32 @@
+import { useState, useEffect } from 'react'
+const Togglable = (props) => {
+    const [visible, setVisible] = useState(false)
+    const hideWhenVisible = { display: visible ? 'none' : '' }
+    const showWhenVisible = { display: visible ? '' : 'none' }
+    const toggleVisibility = () => setVisible(!visible)
+
+    return ( 
+        <>
+            <button className = "viewHideButton" style={hideWhenVisible} onClick={toggleVisibility}>{props.buttonLabel}</button>
+            <button className = "viewHideButton" style={showWhenVisible} onClick={toggleVisibility}>Hide</button>
+            <div style={showWhenVisible}>{props.children}</div>
+        </>
+    )
+}
+
 const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+  <div className="blogPost">
+    <div>
+    {blog.title} : {blog.author}  
+    <Togglable buttonLabel="view">
+        <div>URL: {blog.url}</div>
+        <div>Likes: {blog.likes}<button onClick={()=>console.log(blog.title)}>Likes</button></div>
+        <div>User: {blog.user[0].username}</div>
+    </Togglable>
+    </div>
   </div>  
 )
 
 export default Blog
+
+//<button onClick={()=>console.log(blog.title)}>view</button>
